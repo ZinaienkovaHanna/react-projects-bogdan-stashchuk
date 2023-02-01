15043bf7e9cfab9f6910e9baaa45f4c1931d29a3
Hanna <zinaienkova.anna@gmail.com>
2023-02-01 22:06:47 +0200

    added 10 rendering

---
+++ 10-conditional-rendering/src/components/PetInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PetInfo(props) {
+  const { animal, age, hasPet } = props;
+
+  return hasPet ? (
+    <h1>{`My ${animal} is ${age} years old`}</h1>
+  ) : (
+    <h2>I don't have an animal</h2>
+  );
+}
+
+export default PetInfo;
