7d58b5c9ce6fd2f787b3fffc85315b2b5f419a72
Hanna <zinaienkova.anna@gmail.com>
2023-02-11 14:22:20 +0200

    added 26 react router

--- 26-react-router/src/components/Menu.jsx
+++ 26-react-router/src/components/Menu.jsx
@@ -6,6 +6,7 @@ const Menu = () => {
       <NavLink to="." end>
         Home
       </NavLink>
+      <NavLink to="courses">Courses</NavLink>
       <NavLink to="about">About</NavLink>
       <NavLink to="contacts">Contacts</NavLink>
     </nav>
